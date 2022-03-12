a01f3cd44e30ec7aa77f4df5a24e770027839961
ozkute <101457704+ozkute@users.noreply.github.com>
2022-03-12 13:34:00 +0200

    Add files via upload

---
+++ giedres1.py
@@ -0,0 +1,16 @@
+skaicius = 5
+eilute = "Giedre valgo obuoli"
+simbolis = 'a'
+amogus = 'sussy baka'
+skaiciai=[1,2,3,4,5]
+raides = ['a', 'b', 'c', 'e', 'f']
+raides.insert(3,'d')
+vaikai = ['Gytis', 'Giedre', 'Aistis']
+# raides.insert(5, 'g', 'h', 'i', 'j')
+
+# komentaras 
+# for skaicius in skaiciai:
+#     print(skaicius)
+
+for vaikas in vaikai:
+    print (vaikas + " ziuri pro langa")
